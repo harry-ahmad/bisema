7bf3aa2d40ad3bc796cc70b3f8df91f8c1b583cb
shehrozbarlas799 <shehrozbarlas799@gmail.com>
2021-01-15 01:11:50 +0500

    updated api

--- bisema-api/app/routes/index.js
+++ bisema-api/app/routes/index.js
@@ -1,9 +1,11 @@
-const express           = require('express');
-const router            = express.Router();
-const orderController   = require("../controllers/order.controller");
-const userController   = require("../controllers/user.controller");
+const express               = require('express');
+const router                = express.Router();
+const orderController       = require("../controllers/order.controller");
+const userController        = require("../controllers/user.controller");
+const mailController        = require("../controllers/mail.controller");
 
 router.post("/create",orderController.create);
 router.get("/login/:email/:password",userController.login);
+router.post("/mail",mailController.sendMail);
 
 module.exports = router;
--- bisema-api/server.js
+++ bisema-api/server.js
@@ -4,7 +4,8 @@ const cors       = require("cors");
 const db         = require("./app/models");
 const app        = express();
 const routes     = require("./app/routes");
-
+const fs         = require('fs');
+const path       = require('path');
 var corsOptions  = {
   origin: "*"
 };
@@ -24,11 +25,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // api routes
 app.use('/api',routes);
 
+
+app.use(express.static('public'));
+
+//Serves all the request which includes /images in the url from Images folder
+app.use('/assets', express.static(__dirname + '/assets'));
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  const dirs = path.join(__dirname, '/assets');
+    const f = fs.readdirSync(dirs, {withFileTypes: true})
+    .filter(item => item.isDirectory());
+    res.json({ message: f });
 });
-
+app.get("/:code", (req, res) => {
+  const dirs = path.join(__dirname, `/assets/${req.params.code}`);
+    const f = fs.readdirSync(dirs, {withFileTypes: true})
+    .filter(item => item.isDirectory());
+    res.json({ message: f });
+});
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
